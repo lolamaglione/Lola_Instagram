b7aaecf746564f80e4eb9c84902e18dcd501c2aa
lolamaglione <106709112+lolamaglione@users.noreply.github.com>
2022-06-14 11:59:29 -0700

    adding the launchActivity as a splash screen

--- app/src/main/java/com/lolamaglione/lolainstagram/activities/LaunchActivity.java
+++ app/src/main/java/com/lolamaglione/lolainstagram/activities/LaunchActivity.java
@@ -13,7 +13,20 @@ public class LaunchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
-        goToLoginActivity();
+        Thread background = new Thread() {
+            public void run() {
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(2*1000);
+
+                    // After 5 seconds redirect to another intent
+                    goToLoginActivity();
+                } catch (Exception e) {
+                }
+            }
+        };
+        // start thread
+        background.start();
     }
 
     private void goToLoginActivity(){
